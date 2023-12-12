553bf2a80f783c9230d6285a5186dc41e6e1da67
amumbe <angellamumbe720@gmail.com>
2023-12-12 18:49:10 +0300

    finished database

--- blogApp/views.py
+++ blogApp/views.py
@@ -1,24 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
-posts = [
-    {
-        "author": "AngellaM",
-        "title": "Blog Post 1",
-        "content": "First post content",
-        "date_posted": "january 1, 2023",
-    },
-    {
-        "author": "AngellaB",
-        "title": "Blog Post 2",
-        "content": "First post content",
-        "date_posted": "january 2, 2023",
-    },
-]
-
+from .models import Post
 
 def home(request):
-    context = {"posts": posts}
+    context = {"posts": Post.objects.all()}
 
     return render(request, "blogApp/home.html", context)
 
