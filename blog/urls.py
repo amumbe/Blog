3df979238e66be70645f7b62b6607c0477a3fd9c
amumbe <angellamumbe720@gmail.com>
2024-01-30 20:43:12 +0300

    -M

--- blog/urls.py
+++ blog/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -34,4 +36,4 @@ urlpatterns = [
         name="logout",
     ),
     path("profile/", user_views.profile, name="profile"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
