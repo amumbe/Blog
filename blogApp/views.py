c2dbb13d727cd7ff57e480a973951d7a15f3b2da
amumbe <angellamumbe720@gmail.com>
2024-02-12 21:32:44 +0300

    create posts

--- blogApp/views.py
+++ blogApp/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+
 
 def home(request):
     context = {"posts": Post.objects.all()}
 
     return render(request, "blogApp/home.html", context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blogApp/home.html"
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ["title", "content"]
 
 
 def about(request):
